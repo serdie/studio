6a0d0de5cb3852b83104fdd99fd9b42270d54cf2
Diego Gómez Marín <dginteligenciaartificial@gmail.com>
2026-02-23 16:52:04 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8390159343-1afb2",
+  "appId": "1:677626416097:web:180c8663d8014c0ea7e0a0",
+  "apiKey": "AIzaSyCsg4VnEbnz5bVJnhR2987dyHzdpJ1nV2o",
+  "authDomain": "studio-8390159343-1afb2.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "677626416097"
+};
